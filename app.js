fb7cd003180198ba715e36abb1d925a3264810de
septembrr <43982603+septembrr@users.noreply.github.com>
2020-03-01 20:45:59 -0800

    Updating filter again

--- app.js
+++ app.js
@@ -29,14 +29,23 @@ app.get('/trips',function(req,res,next){
   let queryArgs = [];
 
   if(req.query.filter) {
-    query = "SELECT name, city, country, price, startDate, endDate, Features FROM (SELECT Trips.tripID, Trips.name, Trips.city, Trips.country, Trips.price, Trips.startDate, Trips.endDate, GROUP_CONCAT(DISTINCT Features.name ORDER BY Features.name ASC SEPARATOR ', ') as features FROM Trips LEFT JOIN Trip_Features on Trip_Features.tripID = Trips.tripID LEFT JOIN Features on Features.featureID = Trip_Features.featureID GROUP BY Trips.name) AS trip_options LEFT JOIN (SELECT t.tripID FROM Trips AS t LEFT JOIN Trip_Features AS tf ON tf.tripID = t.tripID LEFT JOIN Features AS f ON f.featureID = tf.featureID WHERE "
-    
-    
-    query += "f.featureID = :featureID_selected_filter AND f.featureID = :featureID_selected_filter_2) AS matching_Trips ON matching_Trips.tripID = trip_options.tripID;";
-
     req.query.feature.array.forEach(element => {
       queryArgs.push(element);
     });
+
+    query = "SELECT name, city, country, price, startDate, endDate, Features FROM (SELECT Trips.tripID, Trips.name, Trips.city, Trips.country, Trips.price, Trips.startDate, Trips.endDate, GROUP_CONCAT(DISTINCT Features.name ORDER BY Features.name ASC SEPARATOR ', ') as features FROM Trips LEFT JOIN Trip_Features on Trip_Features.tripID = Trips.tripID LEFT JOIN Features on Features.featureID = Trip_Features.featureID GROUP BY Trips.name) AS trip_options LEFT JOIN (SELECT t.tripID FROM Trips AS t LEFT JOIN Trip_Features AS tf ON tf.tripID = t.tripID LEFT JOIN Features AS f ON f.featureID = tf.featureID WHERE ";
+    
+    for(let i = 0; i < req.query.filter.length; i++) {
+      queryArgs.push(req.query[i]);
+      query += " f.featureID = " + req.query.filter[i];
+
+      if(i < req.query.filter.length - 1) {
+        query += " AND ";
+      }
+    }
+    
+    query += ") AS matching_Trips ON matching_Trips.tripID = trip_options.tripID;";
+
 
   } else {
     query = "SELECT Trips.name, Trips.city, Trips.country, Trips.price, Trips.startDate, Trips.endDate, GROUP_CONCAT(DISTINCT Features.name ORDER BY Features.name ASC SEPARATOR ', ')  as features FROM Trips LEFT JOIN Trip_Features ON Trips.tripID = Trip_Features.tripID LEFT JOIN Features ON Trip_Features.featureID = Features.featureID GROUP BY Trips.name;";
