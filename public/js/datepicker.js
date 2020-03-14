dfb6e02495a109ea43ec7e42b66b62cdea54e4d1
septembrr <43982603+septembrr@users.noreply.github.com>
2020-03-14 16:25:28 -0700

    Fixing datepicker default date

--- public/js/datepicker.js
+++ public/js/datepicker.js
@@ -1,30 +1,31 @@
 function populateEndDate() {
-    var date2 = jQuery('#startDate').datepicker('getDate');
-    date2.setDate(date2.getDate() + 1);
-    jQuery('#endDate').datepicker('setDate', date2);
-    jQuery("#endDate").datepicker("option", "minDate", date2);
+    var date = jQuery('#startDate').datepicker("getDate");
+    date.setDate(date.getDate() + 1);
+    jQuery('#endDate').datepicker('setDate', date);
+    jQuery("#endDate").datepicker("option", "minDate", date);
 }
 
 jQuery(document).ready(function() {
 
     jQuery("#startDate").datepicker({
-        dateFormat: "MM/dd/yyyy",
+        dateFormat: "yy-mm-dd",
         minDate: 'dateToday',
         onSelect: function(date) {
-        populateEndDate();
+            populateEndDate();
         }
-    }).datepicker("setDate", new Date());
+    });
+
     jQuery('#endDate').datepicker({
-        dateFormat: "MM/dd/yyyy",
+        dateFormat: "yy-mm-dd",
         minDate: 1,
         onClose: function() {
-        var dt1 = jQuery('#startDate').datepicker('getDate');
-        var dt2 = jQuery('#endDate').datepicker('getDate');
-        if (dt2 <= dt1) {
-            var minDate = jQuery('#endDate').datepicker('option', 'minDate');
-            jQuery('#endDate').datepicker('setDate', minDate);
+            var dt1 = jQuery('#startDate').datepicker('getDate');
+            var dt2 = jQuery('#endDate').datepicker('getDate');
+            if (dt2 <= dt1) {
+                var minDate = jQuery('#endDate').datepicker('option', 'minDate');
+                jQuery('#endDate').datepicker('setDate', minDate);
+            }
         }
-        }
-    }).datepicker("setDate", new Date());
+    });
 
 });
